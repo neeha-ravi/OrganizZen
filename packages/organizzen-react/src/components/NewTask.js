8b53aeee91c8d85081d0e2e642fed5250433f4d1
vmagdangal <vmagdangal257@gmail.com>
2023-10-31 18:02:43 -0700

    basis + some react stuff

---
+++ packages/organizzen-react/src/components/NewTask.js
@@ -0,0 +1,85 @@
+import React, {useRef, useState} from 'react';
+import DatePicker from "react-datepicker";
+import './NewTask.css';
+
+function NewTask() {
+  const [popup, popupState] = useState(false);
+  const togglePopup = () => {
+    popupState(!popup);
+  }
+
+  const [date, setDate] = useState('');
+  const dateInputRef = useRef(null);
+  const handleChange = (e) => {
+    setDate(e.target.value);
+  };
+
+  return (
+    <>
+    <button className="popupButton" onClick={togglePopup}>
+      New Task
+    </button>
+
+    {popup && (
+      <div className='popupWindow'>
+      <div className='overlay'></div>
+      <div className='popupContent'>
+        <button id='popupClose' onClick={togglePopup}>CLOSE</button>
+        <h1>New Task</h1>
+        <form className='popupForm'>
+
+          <label htmlFor='taskName'>Name: </label>
+          <br></br>
+          <input
+            id='taskName'
+            name="taskName"
+          />
+          <br></br> <br></br>
+          <label htmlFor='taskDescription'>Description: </label>
+          <br></br>
+          <input
+            id='taskDescription'
+            name="description"
+          />
+
+          <br></br> <br></br>
+          <label htmlFor='taskLink'>Link (Optional): </label>
+          <br></br>
+          <input
+            id='taskDescription'
+            name="description"
+          />
+
+          <br></br> <br></br>
+          <label htmlFor='taskDate'>Deadline: </label>
+          <br></br>
+          <input
+            id='taskDate'
+            type="date"
+            onChange={handleChange}
+            ref={dateInputRef}
+          />
+
+          <br></br> <br></br>
+          <label>
+          Event:
+          <br></br>
+            <select>
+              <option value="event1">event1</option>
+              <option value="event2">event2</option>
+              <option value="event3">event3</option>
+              <option value="event4">event4</option>
+            </select>
+          </label>
+
+          <br></br> <br></br>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    </div>
+    )}
+    </>
+  );
+}
+
+export default NewTask;
