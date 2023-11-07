885c37fbba98a17da5cd998d27b2d5b179302cab
Priya Inampudi <inampudipriya121@gmail.com>
2023-11-06 17:05:47 -0800

    basic backend, does't work

--- packages/organizzen-express/backend.js
+++ packages/organizzen-express/backend.js
@@ -1,52 +1,49 @@
 import express from "express";
 import cors from "cors";
 
-const app = express(); //creates an instance of express
-const port = 8000; //localhost:8000
+const app = express();
+const port = 8000;
 
-app.use(cors());
-
-const events = {
-  events_list: [
-    {
-      name: "Event1",
-      desc: "Description of event 1",
-      date: "2012-04-23",
-    },
-    {
-      name: "Event2",
-      desc: "Description of event 2",
-      date: "2012-04-12",
-    },
-    {
-      name: "Event3",
-      desc: "Description of event 3",
-      date: "2012-05-01",
-    },
-    {
-      name: "Event4",
-      desc: "Description of event 4",
-      date: "2012-04-28",
-    },
-    {
-      name: "Event5",
-      desc: "Description of event 5",
-      date: "2012-03-30",
-    },
-  ],
-};
+// Store events with associated tasks
+const events = [];
 
 const findEventByName = (name) =>
-  events["events_list"].filter((event) => event["name"] === name);
+  events["events"].filter((event) => event["name"] === name);
 
+app.use(cors());
 app.use(express.json()); //set up express to process incoming dats in JSON format
+// Create a new event with tasks
+
+app.post('/events', (req, res) => {
+  const { name, description, link, date, oneDayOnly, tasks } = req.body;
+  const newEvent = { name, description, link, date, oneDayOnly, tasks: [] };
+  events.push(newEvent);
+  res.status(201).json(newEvent);
+});
+
+// Create a new task for an event
+app.post('/events/:eventId/tasks', (req, res) => {
+  const eventId = req.params.eventId;
+  const { name, description, link, date, color } = req.body;
+  const newTask = { name, description, link, date, color };
+  
+  // Find the event by eventId
+  const event = events.find(event => event.name === eventId);
+  if (event) {
+    event.tasks.push(newTask);
+    res.status(201).json(newTask);
+  } else {
+    res.status(404).json({ error: 'Event not found' });
+  }
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!"); //sets the endpoint to accept http GET requests
 });
 
-app.get("/events", (req, res) => {
-  res.send(events);
+// Retrieve events
+app.get('/events', (req, res) => {
+  res.json(events);
 });
 
 app.get("/events/:name", (req, res) => {
