a315e51df3433384cb3cb471e5cb31b0dcd6e223
vmagdangal <vmagdangal257@gmail.com>
2023-11-16 00:02:10 -0800

    NewEvent creates object in backend

--- packages/organizzen-react/src/MyApp.js
+++ packages/organizzen-react/src/MyApp.js
@@ -1,14 +1,35 @@
+import React, { useState } from 'react'
 import HomePage from './components/HomePage'
 import NewEvent from './components/NewEvent'
 import NewTask from './components/NewTask'
 import './MyApp.css'
-
 const App = () => {
+    const [events, setEvents] = useState([])
+
+    function postEvent(event) {
+        const promise = fetch('Http://localhost:8000/events', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(event),
+        })
+        return promise
+    }
+
+    function updateList(event) {
+        postEvent(event)
+            .then(() => setEvents([...events, event]))
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
     return (
         <div>
             <HomePage />
             <div className="newButtons">
-                <NewEvent />
+                <NewEvent handleSubmit={updateList} />
                 <NewTask />
             </div>
         </div>
