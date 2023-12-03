372e91438f338ecf8c4842d33d3c82453e549403
manvsatish <manvsatish@gmail.com>
2023-12-03 13:10:27 -0800

    changes

--- packages/organizzen-express/testdatabase.js
+++ packages/organizzen-express/testdatabase.js
@@ -1,19 +1,23 @@
-const { MongoClient } = require('mongodb');
-require('dotenv').config();
+// database.js
+require('dotenv').config()
+const { MongoClient } = require('mongodb')
 
 const mongoURI = process.env.MONGODB_URI;
 
 const connectToMongoDB = async () => {
-  const client = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+  const client = new MongoClient(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
 
   try {
     await client.connect();
-    console.log('Connected to MongoDB');
-    return client;
+    console.log('Connected to MongoDB')
+    return client
   } catch (err) {
-    console.error('Error connecting to MongoDB:', err);
-    throw err;
+    console.error('Error connecting to MongoDB:', err)
+    throw err
   }
 };
 
-module.exports = { connectToMongoDB };
+export { connectToMongoDB }
