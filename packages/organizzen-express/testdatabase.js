98c39ec25c55de6af6f23cb417f6b337599f3c11
Priya Inampudi <inampudipriya121@gmail.com>
2023-12-03 17:47:10 -0800

    test mongo

--- packages/organizzen-express/testdatabase.js
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv'
-
-// Load environment variables from a .env file
-dotenv.config()
-
-// eslint-disable-next-line no-unused-vars
-const mongoURI = process.env.MONGODB_URI
