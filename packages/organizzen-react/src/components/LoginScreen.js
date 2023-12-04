9d62acb49ca0ca93a6db89fb14cca04efdf42fd4
manvsatish <manvsatish@gmail.com>
2023-12-03 18:54:31 -0800

    updated UI for login/sign up a little

--- packages/organizzen-react/src/components/LoginScreen.js
+++ packages/organizzen-react/src/components/LoginScreen.js
@@ -4,26 +4,28 @@ import './LoginScreen.css'
 
 function LoginScreen() {
     return (
-        <div id="loginScreen">
-            <div className="loginBox">
-                <h1>Welcome back!</h1>
-                <form className="loginForm">
-                    <label htmlFor="username">Username: </label>
+        <div>
+            <div className="loginScreen">
+                <div className="loginBox">
+                    <h1>Welcome back!</h1>
+                    <form className="loginForm">
+                        <label htmlFor="username">Username: </label>
+                        <br></br>
+                        <input id="username" />
+                        <br></br> <br></br>
+                        <label htmlFor="password">Password: </label>
+                        <br></br>
+                        <input id="password" />
+                        <br></br> <br></br>
+                        <input type="submit" value="Login" id="submitform" />
+                    </form>
                     <br></br>
-                    <input id="username" />
-                    <br></br> <br></br>
-                    <label htmlFor="password">Password: </label>
-                    <br></br>
-                    <input id="password" />
-                    <br></br> <br></br>
-                    <input type="submit" value="Login" id="submitform" />
-                </form>
-                <br></br>
-                <p>
-                    <Link to="/signup">
-                        Don't have an account yet? Sign up here!
-                    </Link>
-                </p>
+                    <p>
+                        <Link to="/signup">
+                            Don't have an account yet? Sign up here!
+                        </Link>
+                    </p>
+                </div>
             </div>
         </div>
     )
