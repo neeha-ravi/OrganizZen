d2fc54dd7c9de75a89f14d35ebd4e043f6b967e7
Neeha Ravi <94156134+neeha-ravi@users.noreply.github.com>
2023-12-04 16:41:20 -0800

    Update backend.js

--- packages/organizzen-express/backend.js
+++ packages/organizzen-express/backend.js
@@ -4,7 +4,7 @@ import express from 'express'
 import cors from 'cors'
 
 const app = express()
-const port = 8001
+const port = 8000
 
 app.use(cors())
 app.use(express.json()) // set up express to process incoming data in JSON format
