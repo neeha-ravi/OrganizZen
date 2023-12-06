4aa3029faa4cbd351894e971b7626678d4c183c4
manvsatish <manvsatish@gmail.com>
2023-12-05 20:03:50 -0800

    everything up-to-date with backend working

--- packages/organizzen-react/src/components/TaskTable.js
+++ packages/organizzen-react/src/components/TaskTable.js
@@ -3,7 +3,15 @@ import './TaskTable.css'
 
 function TaskTable({ filter }) {
     const [tasks, setTasks] = useState([])
-
+    const [completedTasks, setCompletedTasks] = useState([])
+    const [showCompleted, setShowCompleted] = useState(
+        localStorage.getItem('showCompleted') === 'true' ? true : false
+    )
+    const [taskDetailsPopup, setTaskDetailsPopupState] = useState(false)
+    const toggleTaskDetailsPopup = () => {
+        setTaskDetailsPopupState(!taskDetailsPopup)
+    }
+    
     useEffect(() => {
         fetch('http://localhost:8000/events/tasks')
             .then((response) => response.json())
@@ -161,31 +169,30 @@ function TaskTable({ filter }) {
         return sortedDates
             .map((date, index) => {
                 const tasksForDate = groupedTasks[date]
-
                 const filteredTasks = tasksForDate.filter(
                     (task) => new Date(task.date) >= currentDate
                 )
 
-            if (filteredTasks.length === 0) {
-                return null
-            }
+                if (filteredTasks.length === 0) {
+                    return null
+                }
 
-            return (
-                <div key={index}>
-                    <div className="DateContainer">
-                        <b>
-                            {new Intl.DateTimeFormat('en-US', {
-                                month: 'long',
-                                day: 'numeric',
-                                year: 'numeric',
-                            }).format(
-                                new Date(
-                                    new Date(date).getTime() +
-                                        24 * 60 * 60 * 1000
-                                )
-                            )}
-                        </b>
-                    </div>
+                return (
+                    <div key={index}>
+                        <div className="DateContainer">
+                            <b>
+                                {new Intl.DateTimeFormat('en-US', {
+                                    month: 'long',
+                                    day: 'numeric',
+                                    year: 'numeric',
+                                }).format(
+                                    new Date(
+                                        new Date(date).getTime() +
+                                            24 * 60 * 60 * 1000
+                                    )
+                                )}
+                            </b>
+                        </div>
 
                         {filteredTasks.map((task) => {
                             const isCompleted = completedTasks.some(
@@ -206,48 +213,106 @@ function TaskTable({ filter }) {
                                         }}
                                     >
                                         <div className="StartText" />
-                                        <div className="TodoItem">
+                                        <div
+                                            className="TodoItem"
+                                            onClick={toggleTaskDetailsPopup}
+                                        >
                                             <label>{task.name}</label>
                                         </div>
                                         <div className="DeleteButtonContainer">
-                                                <button
-                                                  onClick={() => handleDelete(task.id, task.eventId)}
-                                                    className="DeleteButton">🗑️
-                                                  </button>
+                                            <button
+                                                onClick={() =>
+                                                    handleDelete(
+                                                        task.id,
+                                                        task.event
+                                                    )
+                                                }
+                                                className="DeleteButton"
+                                            >
+                                                🗑️
+                                            </button>
                                         </div>
-                                        <div>
-                                            <ViewDetails task={task} />
-                                        </div>
-                                    
+
+                                        {taskDetailsPopup && (
+                                            <div>
+                                                <div className="detailsView">
+                                                    <button
+                                                        id="popupClose"
+                                                        onClick={
+                                                            toggleTaskDetailsPopup
+                                                        }
+                                                    >
+                                                        X
+                                                    </button>
+                                                    <h1>{task.name}</h1>
+                                                    <h2>{task.description}</h2>
+                                                    <hr />
+                                                    {task.link !== '' ? (
+                                                        <h3>
+                                                            Link:{' '}
+                                                            <a href={task.link}>
+                                                                {task.link}
+                                                            </a>
+                                                        </h3>
+                                                    ) : null}
+                                                    <h3>
+                                                        Deadline: {task.date}
+                                                    </h3>
+                                                </div>
+                                            </div>
+                                        )}
+
                                         <div className="CompletedButtonContainer">
                                             <button
                                                 onClick={() =>
                                                     handleDone(
                                                         task.id,
-                                                        task.eventId
+                                                        task.event
                                                     )
                                                 }
                                                 className="CompletedButton"
                                             >
-                                                {task.done ? 'UNDO' : 'DONE'}
+                                                {isCompleted ? 'UNDO' : 'DONE'}
                                             </button>
                                         </div>
-                                        
                                     </div>
                                 )
                             }
                             return null
                         })}
 
-                    {index < sortedDates.length - 1 && (
-                        <div className="Divider"></div>
-                    )}
-                </div>
-            )
+                        {index < sortedDates.length - 1 && (
+                            <div className="Divider"></div>
+                        )}
+                    </div>
+                )
+            })
+            .filter(Boolean)
+    }
+
+    const toggleShowCompleted = () => {
+        setShowCompleted((prevShowCompleted) => {
+            const newShowCompleted = !prevShowCompleted
+            localStorage.setItem('showCompleted', newShowCompleted.toString())
+            return newShowCompleted
         })
     }
 
-    return <div className="ToDoListContainer">{renderTasks()}</div>
+    return (
+        <div className="TaskTableContainer">
+            <div className="ButtonContainer">
+                <button
+                    onClick={toggleShowCompleted}
+                    className="completedtoggle"
+                >
+                    {showCompleted
+                        ? <b>Show Incomplete Tasks</b>
+                        : <b>Show Completed Tasks</b>}
+                </button>
+            </div>
+            <div className="ToDoListContainer">{renderTasks()}</div>
+        </div>
+    )
 }
 
 export default TaskTable
